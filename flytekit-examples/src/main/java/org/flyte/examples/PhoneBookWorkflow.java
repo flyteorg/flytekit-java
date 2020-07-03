a8ef26065aa73c6f1584fe8ba57f1357de9cd6f6
Nelson Arapé <nelson@spotify.com>
2020-07-03 18:52:20 +0200

    Add Task and Workflow to showcase collections and maps

---
+++ flytekit-examples/src/main/java/org/flyte/examples/PhoneBookWorkflow.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.examples;
+
+import com.google.auto.service.AutoService;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.flyte.flytekit.SdkBindingData;
+import org.flyte.flytekit.SdkWorkflow;
+import org.flyte.flytekit.SdkWorkflowBuilder;
+
+@AutoService(SdkWorkflow.class)
+public class PhoneBookWorkflow extends SdkWorkflow {
+
+  private static final List<String> NAMES = Arrays.asList("frodo", "bilbo");
+  private static final Map<String, String> PHONE_BOOK = new HashMap<>();
+
+  static {
+    PHONE_BOOK.put("frodo", "123");
+    PHONE_BOOK.put("bilbo", "456");
+    PHONE_BOOK.put("gandalf", "789");
+  }
+
+  @Override
+  public void expand(SdkWorkflowBuilder builder) {
+    SdkBindingData phoneBook = SdkBindingData.ofStringMap(PHONE_BOOK);
+    SdkBindingData searchKeys = SdkBindingData.ofStringCollection(NAMES);
+
+    SdkBindingData phoneNumbers =
+        builder
+            .mapOf("keyValues", phoneBook, "searchKeys", searchKeys)
+            .apply("search", new BatchLookUpTask())
+            .getOutput("values");
+
+    builder.output("phoneNumbers", phoneNumbers);
+  }
+}

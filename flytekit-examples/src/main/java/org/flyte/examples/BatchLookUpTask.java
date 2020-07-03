7ff7f6cfc26f353ef9585ee5d8c230d2efbc0f1e
Nelson Arapé <nelson@spotify.com>
2020-07-03 18:52:20 +0200

    Add Task and Workflow to showcase collections and maps

---
+++ flytekit-examples/src/main/java/org/flyte/examples/BatchLookUpTask.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.examples;
+
+import com.google.auto.service.AutoService;
+import com.google.auto.value.AutoValue;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.flyte.flytekit.SdkRunnableTask;
+import org.flyte.flytekit.SdkTypes;
+
+/**
+ * Demo task to show case using {@code List<String>} and {@code Map<String,String>} as input and
+ * outputs.
+ */
+@AutoService(SdkRunnableTask.class)
+public class BatchLookUpTask
+    extends SdkRunnableTask<BatchLookUpTask.Input, BatchLookUpTask.Output> {
+  private static final long serialVersionUID = -5702649537830812613L;
+
+  public BatchLookUpTask() {
+    super(SdkTypes.autoValue(Input.class), SdkTypes.autoValue(Output.class));
+  }
+
+  @Override
+  public Output run(Input input) {
+    List<String> foundValues =
+        input.searchKeys().stream()
+            .filter(key -> input.keyValues().containsKey(key))
+            .map(key -> input.keyValues().get(key))
+            .collect(Collectors.toList());
+
+    return Output.create(foundValues);
+  }
+
+  @AutoValue
+  abstract static class Input {
+    abstract Map<String, String> keyValues();
+
+    abstract List<String> searchKeys();
+  }
+
+  @AutoValue
+  abstract static class Output {
+    abstract List<String> values();
+
+    public static Output create(List<String> values) {
+      return new AutoValue_BatchLookUpTask_Output(values);
+    }
+  }
+}

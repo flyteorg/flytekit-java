89972594371b117a243f5ab380ace8cfd41c5d8e
Gleb Kanterov <gleb@spotify.com>
2021-01-07 17:05:17 +0100

    Add SdkLiteralType
    SdkLiteralType is like SdkType, but for individual fields. It's the
    first step for introducing generic T to SdkBindingData, because
    it requires to reason about types of individual fields.

---
+++ flytekit-java/src/main/java/org/flyte/flytekit/SdkLiteralTypes.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.flytekit;
+
+import static java.util.Collections.unmodifiableMap;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.flyte.api.v1.Literal;
+import org.flyte.api.v1.LiteralType;
+import org.flyte.api.v1.Primitive;
+import org.flyte.api.v1.Scalar;
+
+public class SdkLiteralTypes {
+
+  public static SdkLiteralType<Long> integers() {
+    return IntegerSdkLiteralType.INSTANCE;
+  }
+
+  public static SdkLiteralType<Double> floats() {
+    return FloatSdkLiteralType.INSTANCE;
+  }
+
+  public static SdkLiteralType<String> strings() {
+    return StringSdkLiteralType.INSTANCE;
+  }
+
+  public static SdkLiteralType<Boolean> booleans() {
+    return BooleanSdkLiteralType.INSTANCE;
+  }
+
+  public static SdkLiteralType<Instant> datetimes() {
+    return DatetimeSdkLiteralType.INSTANCE;
+  }
+
+  public static SdkLiteralType<Duration> durations() {
+    return DurationSdkLiteralType.INSTANCE;
+  }
+
+  public static <T> SdkLiteralType<List<T>> collections(SdkLiteralType<T> elementType) {
+    return new CollectionSdkLiteralType<>(elementType);
+  }
+
+  public static <T> SdkLiteralType<Map<String, T>> maps(SdkLiteralType<T> mapValueType) {
+    return new MapSdkLiteralType<>(mapValueType);
+  }
+
+  private static class IntegerSdkLiteralType extends SdkLiteralType<Long> {
+    private static final IntegerSdkLiteralType INSTANCE = new IntegerSdkLiteralType();
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralTypes.INTEGER;
+    }
+
+    @Override
+    public Literal toLiteral(Long value) {
+      return Literal.ofScalar(Scalar.ofPrimitive(Primitive.ofInteger(value)));
+    }
+
+    @Override
+    public Long fromLiteral(Literal literal) {
+      return literal.scalar().primitive().integer();
+    }
+  }
+
+  private static class FloatSdkLiteralType extends SdkLiteralType<Double> {
+    private static final FloatSdkLiteralType INSTANCE = new FloatSdkLiteralType();
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralTypes.FLOAT;
+    }
+
+    @Override
+    public Literal toLiteral(Double value) {
+      return Literal.ofScalar(Scalar.ofPrimitive(Primitive.ofFloat(value)));
+    }
+
+    @Override
+    public Double fromLiteral(Literal literal) {
+      return literal.scalar().primitive().float_();
+    }
+  }
+
+  private static class StringSdkLiteralType extends SdkLiteralType<String> {
+    private static final StringSdkLiteralType INSTANCE = new StringSdkLiteralType();
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralTypes.STRING;
+    }
+
+    @Override
+    public Literal toLiteral(String value) {
+      return Literal.ofScalar(Scalar.ofPrimitive(Primitive.ofString(value)));
+    }
+
+    @Override
+    public String fromLiteral(Literal literal) {
+      return literal.scalar().primitive().string();
+    }
+  }
+
+  private static class BooleanSdkLiteralType extends SdkLiteralType<Boolean> {
+    private static final BooleanSdkLiteralType INSTANCE = new BooleanSdkLiteralType();
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralTypes.BOOLEAN;
+    }
+
+    @Override
+    public Literal toLiteral(Boolean value) {
+      return Literal.ofScalar(Scalar.ofPrimitive(Primitive.ofBoolean(value)));
+    }
+
+    @Override
+    public Boolean fromLiteral(Literal literal) {
+      return literal.scalar().primitive().boolean_();
+    }
+  }
+
+  private static class DatetimeSdkLiteralType extends SdkLiteralType<Instant> {
+    private static final DatetimeSdkLiteralType INSTANCE = new DatetimeSdkLiteralType();
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralTypes.DATETIME;
+    }
+
+    @Override
+    public Literal toLiteral(Instant value) {
+      return Literal.ofScalar(Scalar.ofPrimitive(Primitive.ofDatetime(value)));
+    }
+
+    @Override
+    public Instant fromLiteral(Literal literal) {
+      return literal.scalar().primitive().datetime();
+    }
+  }
+
+  private static class DurationSdkLiteralType extends SdkLiteralType<Duration> {
+    private static final DurationSdkLiteralType INSTANCE = new DurationSdkLiteralType();
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralTypes.DURATION;
+    }
+
+    @Override
+    public Literal toLiteral(Duration value) {
+      return Literal.ofScalar(Scalar.ofPrimitive(Primitive.ofDuration(value)));
+    }
+
+    @Override
+    public Duration fromLiteral(Literal literal) {
+      return literal.scalar().primitive().duration();
+    }
+  }
+
+  private static class CollectionSdkLiteralType<T> extends SdkLiteralType<List<T>> {
+    private final SdkLiteralType<T> elementType;
+
+    private CollectionSdkLiteralType(SdkLiteralType<T> elementType) {
+      this.elementType = elementType;
+    }
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralType.ofCollectionType(elementType.getLiteralType());
+    }
+
+    @Override
+    public Literal toLiteral(List<T> value) {
+      List<Literal> collection =
+          value.stream()
+              .map(elementType::toLiteral)
+              .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+
+      return Literal.ofCollection(collection);
+    }
+
+    @Override
+    public List<T> fromLiteral(Literal literal) {
+      return literal.collection().stream()
+          .map(elementType::fromLiteral)
+          .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+    }
+  }
+
+  private static class MapSdkLiteralType<T> extends SdkLiteralType<Map<String, T>> {
+    private final SdkLiteralType<T> mapKeyType;
+
+    private MapSdkLiteralType(SdkLiteralType<T> mapKeyType) {
+      this.mapKeyType = mapKeyType;
+    }
+
+    @Override
+    public LiteralType getLiteralType() {
+      return LiteralType.ofMapValueType(mapKeyType.getLiteralType());
+    }
+
+    @Override
+    public Literal toLiteral(Map<String, T> value) {
+      Map<String, Literal> map = new LinkedHashMap<>();
+
+      for (Map.Entry<String, T> entry : value.entrySet()) {
+        map.put(entry.getKey(), mapKeyType.toLiteral(entry.getValue()));
+      }
+
+      return Literal.ofMap(unmodifiableMap(map));
+    }
+
+    @Override
+    public Map<String, T> fromLiteral(Literal literal) {
+      Map<String, T> map = new LinkedHashMap<>();
+
+      for (Map.Entry<String, Literal> entry : literal.map().entrySet()) {
+        map.put(entry.getKey(), mapKeyType.fromLiteral(entry.getValue()));
+      }
+
+      return unmodifiableMap(map);
+    }
+  }
+}

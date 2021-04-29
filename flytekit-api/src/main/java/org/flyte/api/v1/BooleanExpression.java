166d5d9592893163c3ca9c01cc6d16a676474a5c
Gleb Kanterov <gleb@spotify.com>
2021-04-29 11:31:09 +0200

    Add BranchNode to flytekit-api
    Only flytekit-api part, serialization and local execution isn't
    implemented yet.
    
    Signed-off-by: Gleb Kanterov <gleb@spotify.com>

---
+++ flytekit-api/src/main/java/org/flyte/api/v1/BooleanExpression.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.api.v1;
+
+import com.google.auto.value.AutoOneOf;
+
+/**
+ * Defines a boolean expression tree. It can be a simple or a conjunction expression. Multiple
+ * expressions can be combined using a conjunction or a disjunction to result in a final boolean
+ * result.
+ */
+@AutoOneOf(BooleanExpression.Kind.class)
+public abstract class BooleanExpression {
+  public enum Kind {
+    CONJUNCTION,
+    COMPARISON
+  }
+
+  public abstract Kind kind();
+
+  public abstract ComparisonExpression comparison();
+
+  public abstract ConjunctionExpression conjunction();
+
+  public static BooleanExpression ofComparison(ComparisonExpression comparison) {
+    return AutoOneOf_BooleanExpression.comparison(comparison);
+  }
+
+  public static BooleanExpression ofConjunction(ConjunctionExpression conjunction) {
+    return AutoOneOf_BooleanExpression.conjunction(conjunction);
+  }
+}

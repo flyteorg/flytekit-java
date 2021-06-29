464d01c5934deca511f88233a7bdf22530bd6578
Sean Lin <sean@union.ai>
2021-06-29 09:38:56 -0700

    Apply spotless
    Signed-off-by: Sean Lin <sean@union.ai>

--- flytekit-examples-scala/src/main/scala/org/flyte/examples/flytekitscala/AddQuestionTask.scala
+++ flytekit-examples-scala/src/main/scala/org/flyte/examples/flytekitscala/AddQuestionTask.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.flyte.examples.flytekitscala
 
 import org.flyte.flytekit.{SdkBindingData, SdkRunnableTask, SdkTransform}
@@ -7,9 +23,9 @@ case class AddQuestionTaskInput(greeting: String)
 case class AddQuestionTaskOutput(greeting: String)
 
 /**
- * Example Flyte task that takes a greeting message as input, appends "How are you?", and outputs
- * the result
- */
+  * Example Flyte task that takes a greeting message as input, appends "How are you?", and outputs
+  * the result
+  */
 class AddQuestionTask
     extends SdkRunnableTask(
       SdkScalaType[AddQuestionTaskInput],
@@ -17,22 +33,24 @@ class AddQuestionTask
     ) {
 
   /**
-   * Defines task behavior. This task takes a greeting message as the input, append it with " How
-   * are you?", and outputs a new greeting message.
-   *
-   * @param input the greeting message
-   * @return the updated greeting message
-   */
-  override def run(input: AddQuestionTaskInput): AddQuestionTaskOutput = AddQuestionTaskOutput(s"${input.greeting} How are you?")
+    * Defines task behavior. This task takes a greeting message as the input, append it with " How
+    * are you?", and outputs a new greeting message.
+    *
+    * @param input the greeting message
+    * @return the updated greeting message
+    */
+  override def run(input: AddQuestionTaskInput): AddQuestionTaskOutput =
+    AddQuestionTaskOutput(s"${input.greeting} How are you?")
 }
 
 object AddQuestionTask {
+
   /**
-   * Binds input data to this task
-   *
-   * @param greeting the input greeting message
-   * @return a transformed instance of this class with input data
-   */
+    * Binds input data to this task
+    *
+    * @param greeting the input greeting message
+    * @return a transformed instance of this class with input data
+    */
   def apply(greeting: SdkBindingData): SdkTransform =
     new AddQuestionTask().withInput("greeting", greeting)
 }
--- flytekit-examples-scala/src/main/scala/org/flyte/examples/flytekitscala/GreetTask.scala
+++ flytekit-examples-scala/src/main/scala/org/flyte/examples/flytekitscala/GreetTask.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.flyte.examples.flytekitscala
 
 import org.flyte.flytekit.{SdkBindingData, SdkRunnableTask, SdkTransform}
@@ -14,22 +30,24 @@ class GreetTask
     ) {
 
   /**
-   * Defines task behavior. This task takes a name as the input, wraps it in a welcome message, and
-   * outputs the message.
-   *
-   * @param input the name of the person to be greeted
-   * @return the welcome message
-   */
-  override def run(input: GreetTaskInput): GreetTaskOutput = GreetTaskOutput(s"Welcome, ${input.name}!")
+    * Defines task behavior. This task takes a name as the input, wraps it in a welcome message, and
+    * outputs the message.
+    *
+    * @param input the name of the person to be greeted
+    * @return the welcome message
+    */
+  override def run(input: GreetTaskInput): GreetTaskOutput =
+    GreetTaskOutput(s"Welcome, ${input.name}!")
 }
 
 object GreetTask {
+
   /**
-   * Binds input data to this task
-   *
-   * @param name the input name
-   * @return 1 transformed instance of this class with input data
-   */
+    * Binds input data to this task
+    *
+    * @param name the input name
+    * @return a transformed instance of this class with input data
+    */
   def apply(name: SdkBindingData): SdkTransform =
     new GreetTask().withInput("name", name)
 }

a4e67cdbb68d7719d5ed10d396519c97adadacce
Gleb Kanterov <gleb@spotify.com>
2021-08-04 13:43:16 +0200

    Add integration tests
    Signed-off-by: Nelson Arapé <narape@users.noreply.github.com>

---
+++ integration-tests/src/main/java/org/flyte/integrationtests/FailingTask.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.integrationtests;
+
+import com.google.auto.service.AutoService;
+import org.flyte.flytekit.SdkRunnableTask;
+import org.flyte.flytekit.SdkTypes;
+
+@AutoService(SdkRunnableTask.class)
+public class FailingTask extends SdkRunnableTask<Void, Void> {
+  private static final long serialVersionUID = 42L;
+
+  public static FailingTask of() {
+    return new FailingTask();
+  }
+
+  public FailingTask() {
+    super(SdkTypes.nulls(), SdkTypes.nulls());
+  }
+
+  @Override
+  public Void run(Void input) {
+    throw new RuntimeException("task failed");
+  }
+}

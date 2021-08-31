ba2351c042375df1b19df056e8779b9fa826fdf0
Rajat <rajat.yadav@zenways.io>
2021-08-31 18:44:48 +0530

    comment updated and job name changed
    Signed-off-by: Rajat <rajat.yadav@zenways.io>

--- scripts/update_flyteidl.sh
+++ scripts/update_flyteidl.sh
@@ -1,9 +1,4 @@
-#!/usr/bin/env bash
-
-# WARNING: THIS FILE IS MANAGED IN THE 'BOILERPLATE' REPO AND COPIED TO OTHER REPOSITORIES.
-# ONLY EDIT THIS FILE FROM WITHIN THE 'FLYTEORG/BOILERPLATE' REPOSITORY:
-# 
-# TO OPT OUT OF UPDATES, SEE https://github.com/flyteorg/boilerplate/blob/master/Readme.rst
+#copying latest proto from flyteidl to flytekit-java
 
 set -e
 

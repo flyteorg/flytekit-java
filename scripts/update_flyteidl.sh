0fc6bb3a71c671762e31347a9413abee52ba1fac
Yuvraj <code@evalsocket.dev>
2021-11-24 15:43:30 +0530

    Added automation for flyteidl (#40)
    * Added automation for flyteidl
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * mvn spotless:apply
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>

---
+++ scripts/update_flyteidl.sh
@@ -0,0 +1,17 @@
+#copying latest proto from flyteidl to flytekit-java
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+OUT="$(mktemp -d)"
+trap 'rm -fr $OUT' EXIT
+
+
+FLYTEIDL_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flyteidl/releases/latest" | jq -r .tag_name)
+git clone https://github.com/flyteorg/flyteidl.git "${OUT}" --branch "${FLYTEIDL_VERSION}"
+
+for dir in "admin" "core" "event" "service"
+do
+    mv ${OUT}/protos/flyteidl/${dir}/*  jflyte/src/main/proto/flyteidl/${dir}/
+done

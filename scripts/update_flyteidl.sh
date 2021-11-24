0dc2ba5618ace8154edd646f72668938b41da9e3
Yuvraj <code@evalsocket.dev>
2021-11-24 15:50:36 +0530

    Update proto path in script
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- scripts/update_flyteidl.sh
+++ scripts/update_flyteidl.sh
@@ -13,5 +13,5 @@ git clone https://github.com/flyteorg/flyteidl.git "${OUT}" --branch "${FLYTEIDL
 
 for dir in "admin" "core" "event" "service"
 do
-    mv ${OUT}/protos/flyteidl/${dir}/*  jflyte/src/main/proto/flyteidl/${dir}/
+    mv ${OUT}/protos/flyteidl/${dir}/*  flyteidl-protos/src/main/proto/flyteidl/${dir}/
 done

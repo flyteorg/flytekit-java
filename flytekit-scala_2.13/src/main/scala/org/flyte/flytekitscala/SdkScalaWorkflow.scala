dcbb337fa68dda020bf860374f725ca742e0509f
Andrés Gómez <andresgomezfrr@gmail.com>
2023-02-02 16:18:34 +0100

    Add scaladocs #patch (#185)
    * Add scaladocs
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>
    
    * Add missing scaladocs
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>
    
    * Fix spotless
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>
    
    ---------
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>

--- flytekit-scala_2.13/src/main/scala/org/flyte/flytekitscala/SdkScalaWorkflow.scala
+++ flytekit-scala_2.13/src/main/scala/org/flyte/flytekitscala/SdkScalaWorkflow.scala
@@ -28,6 +28,19 @@ import org.flyte.flytekit.{
 
 import scala.collection.JavaConverters._
 
+/** This class is used to implement a Flyte workflow. This is an abstract class
+  * so you need to override and implement the expand method, where you need to
+  * configure the workflow logic.
+  *
+  * @param inputType
+  *   it should be SdkScalaType[InputT].
+  * @param outputType
+  *   it should be SdkScalaType[OutputT].
+  * @tparam InputT
+  *   Input case class.
+  * @tparam OutputT
+  *   Output case class.
+  */
 abstract class SdkScalaWorkflow[InputT, OutputT](
     inputType: SdkType[InputT],
     outputType: SdkType[OutputT]
@@ -39,39 +52,114 @@ abstract class SdkScalaWorkflow[InputT, OutputT](
     expand(new SdkScalaWorkflowBuilder(builder), input)
   }
 
+  /** The expand method must be implement by the workflow developer. The
+    * workflow developer must coding the workflow logic on this method.
+    * @param builder
+    *   The builder which is used to build the workflow DAG.
+    * @param input
+    *   The workflow input.
+    * @return
+    *   The workflow output.
+    */
   def expand(builder: SdkScalaWorkflowBuilder, input: InputT): OutputT
 }
 
 class SdkScalaWorkflowBuilder(builder: SdkWorkflowBuilder) {
 
+  /** Get the nodes applied on the DAG.
+    * @return
+    *   The workflows node by name.
+    */
   def getNodes: Map[String, SdkNode[_]] = builder.getNodes.asScala.toMap
 
+  /** Get a map with name/value of inputs.
+    * @return
+    *   The workflow inputs by name.
+    */
   def getInputs: Map[String, SdkJavaBindingData[_]] =
     builder.getInputs.asScala.toMap
 
+  /** Get an specific input description.
+    *
+    * @return
+    *   The input description.
+    */
   def getInputDescription(name: String): String =
     builder.getInputDescription(name)
 
+  /** Get an specific output description.
+    *
+    * @return
+    *   The output description.
+    */
   def getOutputDescription(name: String): String =
     builder.getOutputDescription(name)
 
+  /** Transform the current DAG to [[WorkflowTemplate]]
+    * @return
+    *   A workflow template
+    */
   def toIdlTemplate: WorkflowTemplate = builder.toIdlTemplate
 
+  /** Create a new node without inputs and specific ID on the workflow DAG.
+    *
+    * @param nodeId
+    *   The specific node ID.
+    * @param transform
+    *   The transformation that you want to apply to the DAG.
+    * @tparam OutputT
+    *   The [[SdkTransform]] and [[SdkNode]] output class.
+    * @return
+    */
   def apply[OutputT](
       nodeId: String,
       transform: SdkTransform[Unit, OutputT]
   ): SdkNode[OutputT] =
     builder.apply(nodeId, transform, ())
 
+  /** Create a new node with specific ID on the workflow DAG.
+    *
+    * @param nodeId
+    *   The specific node ID.
+    * @param transform
+    *   The transformation that you want to apply to the DAG.
+    * @param inputs
+    *   The [[SdkTransform]] inputs.
+    * @tparam InputT
+    *   The [[SdkTransform]] input class.
+    * @tparam OutputT
+    *   The [[SdkTransform]] and [[SdkNode]] output class.
+    * @return
+    */
   def apply[InputT, OutputT](
       nodeId: String,
       transform: SdkTransform[InputT, OutputT],
       inputs: InputT
   ): SdkNode[OutputT] = builder.apply(nodeId, transform, inputs)
 
+  /** Create a new node without inputs on the workflow DAG.
+    *
+    * @param transform
+    *   The transformation that you want to apply to the DAG.
+    * @tparam OutputT
+    *   The [[SdkTransform]] and [[SdkNode]] output class.
+    * @return
+    */
   def apply[OutputT](transform: SdkTransform[Unit, OutputT]): SdkNode[OutputT] =
     builder.apply(transform, ())
 
+  /** Create a new node on the workflow DAG.
+    *
+    * @param transform
+    *   The transformation that you want to apply to the DAG.
+    * @param inputs
+    *   The [[SdkTransform]] inputs.
+    * @tparam InputT
+    *   The [[SdkTransform]] input class.
+    * @tparam OutputT
+    *   The [[SdkTransform]] and [[SdkNode]] output class.
+    * @return
+    */
   def apply[InputT, OutputT](
       transform: SdkTransform[InputT, OutputT],
       inputs: InputT

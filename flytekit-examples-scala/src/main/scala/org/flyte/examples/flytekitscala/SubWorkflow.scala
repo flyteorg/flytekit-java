cc03123771385d9086c5c6a711d661a1de2151e2
Sonja Ericsson <sonjae@spotify.com>
2023-03-10 09:05:58 +0100

    add scala subworkflow ex
    Signed-off-by: Sonja Ericsson <sonjae@spotify.com>

---
+++ flytekit-examples-scala/src/main/scala/org/flyte/examples/flytekitscala/SubWorkflow.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Flyte Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.examples.flytekitscala
+
+import org.flyte.flytekitscala.{
+  SdkScalaType,
+  SdkScalaWorkflow,
+  SdkScalaWorkflowBuilder
+}
+
+class SubWorkflow
+    extends SdkScalaWorkflow[WelcomeWorkflowInput, WelcomeWorkflowOutput](
+      SdkScalaType[WelcomeWorkflowInput],
+      SdkScalaType[WelcomeWorkflowOutput]
+    ) {
+
+  override def expand(
+      builder: SdkScalaWorkflowBuilder,
+      input: WelcomeWorkflowInput
+  ): WelcomeWorkflowOutput = {
+
+    val greeting = builder
+      .apply("greet", new WelcomeWorkflow(), WelcomeWorkflowInput(input.name))
+      .getOutputs
+      .greeting
+
+    WelcomeWorkflowOutput(greeting)
+  }
+}

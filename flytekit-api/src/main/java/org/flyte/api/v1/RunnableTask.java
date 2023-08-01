1850f16a2aac104f924a8a308ffceba4702afb24
Andrés Gómez <andresgomezfrr@gmail.com>
2023-08-01 09:58:21 +0200

    Add custom JAVA_TOOL_OPTIONS by SdkRunnableTask (#238)
    * Add custom JAVA_TOOL_OPTIONS by SdkRunnableTask
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>
    
    * Address PR comments
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>
    
    ---------
    
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>

--- flytekit-api/src/main/java/org/flyte/api/v1/RunnableTask.java
+++ flytekit-api/src/main/java/org/flyte/api/v1/RunnableTask.java
@@ -18,23 +18,26 @@ package org.flyte.api.v1;
 
 import static java.util.Collections.emptyMap;
 
+import java.util.List;
+
 /** Building block for tasks that execute Java code. */
 public interface RunnableTask extends Task, RunnableNode {
 
   @Override
   default String getType() {
-    // FIXME default only for backwards-compatibility, remove in 0.3.x
     return "java-task";
   }
 
   @Override
   default Struct getCustom() {
-    // FIXME default only for backwards-compatibility, remove in 0.3.x
     return Struct.of(emptyMap());
   }
 
   default Resources getResources() {
-    // FIXME default only for backwards-compatibility, remove in 0.3.x
     return Resources.builder().build();
+  }
+
+  default List<String> getCustomJavaToolOptions() {
+    return List.of();
   }
 }

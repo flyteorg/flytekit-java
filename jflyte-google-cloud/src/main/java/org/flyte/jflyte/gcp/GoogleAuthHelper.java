7bc1dc742c0f7e6f877ac0e9f6f4f7f737cd729e
Hongxin Liang <honnix@users.noreply.github.com>
2023-09-28 09:29:52 +0200

    Retro fix all years
    Signed-off-by: Hongxin Liang <honnix@users.noreply.github.com>

--- jflyte-google-cloud/src/main/java/org/flyte/jflyte/gcp/GoogleAuthHelper.java
+++ jflyte-google-cloud/src/main/java/org/flyte/jflyte/gcp/GoogleAuthHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Flyte Authors
+ * Copyright 2021-2022 Flyte Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
